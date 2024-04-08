171599036c7e2d21e565a05d35f7450454d66000
stefanli2577 <163996174+stefanli2577@users.noreply.github.com>
2024-04-08 15:20:47 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,12 @@
-
-
 import random
 import re
 
 with open('README.md', 'r') as f:
     content = f.read()
 
-rand_num = random.randint(1, 1000000000)
+rand_str = '<!-- {} -->'.format(random.randint(1, 1000000000))
 
-content = re.sub('<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->', f'<!-- BLOG_START -->\n{rand_num}\n<!-- BLOG_END -->', content)
+content = re.sub('<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->', f'<!-- BLOG_START -->\n{rand_str}\n<!-- BLOG_END -->', content)
 
 with open('README.md', 'w') as f:
     f.write(content)
-
